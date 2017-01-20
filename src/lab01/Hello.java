355b90c1cd6f56c5c61f84f596c4e153ffbe8ca7
Yuxuan Huang <yh009@bucknell.edu>
2017-01-20 01:21:15 -0500

    Lab01, Exercise 3, Complete

---
+++ src/lab01/Hello.java
@@ -0,0 +1,23 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+* Instructor: Prof Christopher Dancy
+* Section: 1 pm
+*
+* Name: Yuxuan Huang
+* Date: 01/19/2017
+*
+* Lab / Assignment: Lab01
+*
+* Description: A "Hello World" program in Java.
+*
+* *****************************************/
+
+package lab01;
+public class Hello
+{
+	public static void main( String[] args ){
+		System.out.println("Programming is not a spectator sport!");
+		System.exit(0);
+	}
+}
