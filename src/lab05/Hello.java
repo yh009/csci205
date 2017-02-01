b3a590d18d57eaa12efcffa49bb0802ebf5a9f04
Yuxuan Huang <yh009@bucknell.edu>
2017-02-01 16:52:27 -0500

    Lab05, exercise 1, a simple hello world program.

---
+++ src/lab05/Hello.java
@@ -0,0 +1,32 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2017
+*
+* Name: YUXUAN HUANG
+* Date: Feb 1, 2017
+* Time: 4:31:57 PM
+*
+* Project: csci205s
+* Package: lab05
+* File: Hello
+* Description: This is a Hello World java classs
+*
+* ****************************************
+ */
+package lab05;
+
+/**
+ *
+ * @author Yuxuan Huang
+ * @version 0.1
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Becoming a good programmer takes practice.");
+    }
+
+}
