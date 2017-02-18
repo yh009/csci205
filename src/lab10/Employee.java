9af034b395701cdb023b4ea8be9e341d2fb64e69
Yuxuan Huang <yh009@bucknell.edu>
2017-02-17 21:04:46 -0500

    lab10 prep

---
+++ src/lab10/Employee.java
@@ -0,0 +1,136 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2016
+ *
+ * TODO - Enter the details below!
+ * Name: TODO
+ * Date: TODO
+ * Time: TODO
+ *
+ * Project: csci205
+ * Package: lab10
+ * File: Employee
+ * Description:
+ * A very basic abstraction for an employee in a simple HR system
+ * ****************************************
+ */
+package lab10;
+
+/**
+ * A basic representation for an Employee.
+ *
+ * @author Brian King
+ */
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Employee - represents an employee in the database system
+ *
+ * @author brk009
+ */
+public class Employee {
+
+    private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MMM-dd");
+
+    private int empID;
+    private String firstName;
+    private String lastName;
+    private int ssNum;
+    private Date hireDate;
+    private double salary;
+
+    /**
+     * Explicit construct to create new employee
+     *
+     * @param empID
+     * @param firstName
+     * @param lastName
+     * @param ssNum
+     * @param hireDate
+     * @param salary
+     */
+    public Employee(int empID, String firstName, String lastName, int ssNum, Date hireDate, double salary) {
+        this.empID = empID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssNum = ssNum;
+        this.hireDate = hireDate;
+        this.salary = salary;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSsNum() {
+        return ssNum;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * Change the name of the employee
+     *
+     * @param first - New first name
+     * @param last - New last name
+     */
+    public void changeName(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+
+    /**
+     * Raise the salary by <code>salaryAdj</code> dollars.
+     *
+     * @param salaryAdj - amount to add to the current salary
+     * @return the new salary
+     */
+    public double raiseSalary(double salaryAdj) {
+        this.salary += salaryAdj;
+        return this.salary;
+    }
+
+
+    /**
+     * Helper method to parse a date string into a date object. This is
+     * really here just to show how to deal with an exception that may
+     * be thrown in a method.
+     *
+     * @param sDate - a date string
+     * @return a <code>Date</code> object
+     * @throws ParseException if the string cannot be parse correctly.
+     */
+    public static Date parseHireDate(String sDate) throws ParseException {
+        return df.parse(sDate);
+    }
+
+    /**
+     * Return a string representation of the Employee
+     *
+     * @return the String of comma delimited values
+     */
+    @Override
+    public String toString() {
+        String s = this.empID + "," + this.lastName + "," + this.firstName;
+        s += String.format(",%09d", this.ssNum);
+        s += "," + df.format(this.hireDate);
+        s += String.format(",%.2f", this.salary);
+        return s;
+    }
+}
